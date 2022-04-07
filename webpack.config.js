b5a3ad8de967d679f73400487392321ad464b5ef
jinhopkg <jinhopkg@gmail.com>
2022-04-07 11:32:46 +0900

    chore: Update load env type

--- webpack.config.js
+++ webpack.config.js
@@ -2,64 +2,72 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
 const dotenv = require("dotenv");
-dotenv.config();
 
-module.exports = {
-  entry: {
-    main: "./src/index.tsx"
-  },
-  devtool: "inline-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: [
-          {
-            loader: "ts-loader",
-            options: {
-              compilerOptions: { noEmit: false }
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    dotenv.config({ path: ".env.development" });
+  } else {
+    dotenv.config();
+  }
+
+  return {
+    mode: argv.mode,
+    entry: {
+      main: "./src/index.tsx"
+    },
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: [
+            {
+              loader: "ts-loader",
+              options: {
+                compilerOptions: { noEmit: false }
+              }
             }
-          }
-        ],
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+          ],
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      fallback: {
+        buffer: false,
+        crypto: false,
+        events: false,
+        path: false,
+        stream: false,
+        string_decoder: false
       }
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    devServer: {
+      port: 3000,
+      hot: true,
+      historyApiFallback: true,
+      liveReload: true
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./public/index.html",
+        chunks: ["main"]
+      }),
+      new webpack.DefinePlugin({
+        "process.env": JSON.stringify(process.env)
+      }),
+      new webpack.ProvidePlugin({
+        Buffer: ["buffer", "Buffer"]
+      })
     ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    fallback: {
-      buffer: false,
-      crypto: false,
-      events: false,
-      path: false,
-      stream: false,
-      string_decoder: false
-    }
-  },
-  output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "dist")
-  },
-  devServer: {
-    port: 3000,
-    hot: true,
-    historyApiFallback: true,
-    liveReload: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-      chunks: ["main"]
-    }),
-    new webpack.DefinePlugin({
-      "process.env": JSON.stringify(process.env)
-    }),
-    new webpack.ProvidePlugin({
-      Buffer: ["buffer", "Buffer"]
-    })
-  ]
+  };
 };
